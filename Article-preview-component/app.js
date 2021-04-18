5539e679c34c6d366bc479dc646f29841844f4ab
os-rooney <rooney@rooos.de>
2021-04-18 18:55:12 +0200

    edit Readme file, correcting the mobile active version for share menu

--- Article-preview-component/app.js
+++ Article-preview-component/app.js
@@ -2,21 +2,27 @@ const shareOpen = document.getElementById('share-open');
 const shareClose = document.getElementById('share-close');
 const footer = document.querySelector('.footer');
 const shareBar = document.querySelector('.share-bar'); 
-
+const avatar = document.querySelector('.avatar');
+const author = document.querySelector('.author');
+const OpenButton = document.querySelector('.actionDestop-open');
 
 function share() {
     // open and clode social media sharin bar depending on screen size
-    if(window.innerWidth < 375){
+    if(window.innerWidth < 376){
         // open social media sharing bar
         shareOpen.addEventListener('click', ()=> {
-            footer.classList.toggle('hide-footer');
+            OpenButton.classList.toggle('hide');
             shareBar.classList.toggle('hide-sharebar');
+            avatar.classList.toggle('hide');
+            author.classList.toggle('hide');        
         })
 
         // open social media sharing bar
         shareClose.addEventListener('click', ()=> {
-            footer.classList.toggle('hide-footer');
+            OpenButton.classList.toggle('hide');
             shareBar.classList.toggle('hide-sharebar');
+            avatar.classList.toggle('hide');
+            author.classList.toggle('hide');
         })
     } else {
         // open social media sharing bar
